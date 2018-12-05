7febced6e1cdf292b83cca9b0388429544e95d7b
Vadim Arasev <varasev@gmail.com>
2018-12-05 20:30:57 +0300

    Simplify `KeyGenHistory` contract

--- contracts/KeyGenHistory.sol
+++ contracts/KeyGenHistory.sol
@@ -19,9 +19,8 @@ contract KeyGenHistory is EternalStorage {
 
     event AckWritten(
         address indexed validator,
-        bytes32 indexed hashOfPart,
         bytes ack,
-        uint256 stakingEpoch,
+        uint256 indexed stakingEpoch,
         uint256 indexed changeRequestCount
     );
 
@@ -45,112 +44,53 @@ contract KeyGenHistory is EternalStorage {
         addressStorage[VALIDATOR_SET] = _validatorSet;
     }
 
+    // Note: since this is non-system transaction, the calling validator
+    // should have enough balance to call this function.
     function writePart(bytes _part) public onlyValidator {
+        require(!validatorWrotePart(changeRequestCount, msg.sender));
+
+        _setValidatorWrotePart(changeRequestCount, msg.sender);
+
         IReportingValidatorSet validatorSetContract = validatorSet();
 
-        bytes32 hashOfPart = keccak256(_part);
         uint256 stakingEpoch = validatorSetContract.stakingEpoch();
         uint256 changeRequestCount = validatorSetContract.changeRequestCount();
-
-        require(validatorPart(changeRequestCount, msg.sender) == bytes32(0));
-
-        _setValidatorPart(changeRequestCount, msg.sender, hashOfPart);
 
         emit PartWritten(msg.sender, _part, stakingEpoch, changeRequestCount);
     }
 
+    // Note: since this is non-system transaction, the calling validator
+    // should have enough balance to call this function.
     function writeAck(bytes _ack) public onlyValidator {
         IReportingValidatorSet validatorSetContract = validatorSet();
 
         uint256 stakingEpoch = validatorSetContract.stakingEpoch();
         uint256 changeRequestCount = validatorSetContract.changeRequestCount();
-        
-        bytes32 hashOfPart = validatorPart(changeRequestCount, msg.sender);
-        bytes32 hashOfAck = keccak256(_ack);
 
-        require(hashOfPart != bytes32(0));
-        require(!partAckExists(changeRequestCount, hashOfPart, hashOfAck));
-
-        _pushPartAck(changeRequestCount, hashOfPart, hashOfAck);
-        _setPartAckExists(changeRequestCount, hashOfPart, hashOfAck);
-
-        emit AckWritten(msg.sender, hashOfPart, _ack, stakingEpoch, changeRequestCount);
+        emit AckWritten(msg.sender, _ack, stakingEpoch, changeRequestCount);
     }
 
     // =============================================== Getters ========================================================
 
-    function isKeyGenComplete() public view returns(bool) {
-        IReportingValidatorSet validatorSetContract = validatorSet();
-
-        address[] memory validators = validatorSetContract.getValidators();
-        uint256 validatorsLength = validators.length;
-        
-        uint256 changeRequestCount = validatorSetContract.changeRequestCount();
-        uint256 partsReceivedEnoughAcks = 0;
-
-        for (uint256 i = 0; i < validatorsLength; i++) {
-            address validator = validators[i];
-            bytes32 hashOfPart = validatorPart(changeRequestCount, validator);
-            uint256 acksReceived = partAcks(changeRequestCount, hashOfPart).length;
-            
-            if (acksReceived.mul(3) >= validatorsLength) {
-                partsReceivedEnoughAcks++;
-            }
-        }
-
-        return partsReceivedEnoughAcks.mul(3) > validatorsLength.mul(2);
-    }
-
-    function partAcks(uint256 _changeRequestCount, bytes32 _hashOfPart) public view returns(bytes32[]) {
-        return bytes32ArrayStorage[
-            keccak256(abi.encode(PART_ACKS, _changeRequestCount, _hashOfPart))
-        ];
-    }
-
-    function partAckExists(uint256 _changeRequestCount, bytes32 _hashOfPart, bytes32 _hashOfAck)
-        public
-        view
-        returns(bool)
-    {
-        return boolStorage[
-            keccak256(abi.encode(PART_ACK_EXISTS, _changeRequestCount, _hashOfPart, _hashOfAck))
-        ];
-    }
-
-    function validatorPart(uint256 _changeRequestCount, address _validator) public view returns(bytes32) {
-        return bytes32Storage[
-            keccak256(abi.encode(VALIDATOR_PART, _changeRequestCount, _validator))
-        ];
-    }
-
     function validatorSet() public view returns(IReportingValidatorSet) {
         return IReportingValidatorSet(addressStorage[VALIDATOR_SET]);
+    }
+
+    function validatorWrotePart(uint256 _changeRequestCount, address _validator) public view returns(bool) {
+        return boolStorage[
+            keccak256(abi.encode(VALIDATOR_WROTE_PART, _changeRequestCount, _validator))
+        ];
     }
 
     // =============================================== Private ========================================================
 
     bytes32 internal constant OWNER = keccak256("owner");
     bytes32 internal constant VALIDATOR_SET = keccak256("validatorSet");
+    bytes32 internal constant VALIDATOR_WROTE_PART = "validatorWrotePart";
 
-    bytes32 internal constant PART_ACKS = "partAcks";
-    bytes32 internal constant PART_ACK_EXISTS = "partAckExists";
-    bytes32 internal constant VALIDATOR_PART = "validatorPart";
-
-    function _pushPartAck(uint256 _changeRequestCount, bytes32 _hashOfPart, bytes32 _hashOfAck) internal {
-        bytes32ArrayStorage[
-            keccak256(abi.encode(PART_ACKS, _changeRequestCount, _hashOfPart))
-        ].push(_hashOfAck);
-    }
-
-    function _setPartAckExists(uint256 _changeRequestCount, bytes32 _hashOfPart, bytes32 _hashOfAck) internal {
+    function _setValidatorWrotePart(uint256 _changeRequestCount, address _validator) internal {
         boolStorage[
-            keccak256(abi.encode(PART_ACK_EXISTS, _changeRequestCount, _hashOfPart, _hashOfAck))
+            keccak256(abi.encode(VALIDATOR_WROTE_PART, _changeRequestCount, _validator))
         ] = true;
-    }
-
-    function _setValidatorPart(uint256 _changeRequestCount, address _validator, bytes32 _hashOfPart) internal {
-        bytes32Storage[
-            keccak256(abi.encode(VALIDATOR_PART, _changeRequestCount, _validator))
-        ] = _hashOfPart;
     }
 }
