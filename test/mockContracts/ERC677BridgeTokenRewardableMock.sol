50d5b096d55f2c6078d4fc1054a5489d4a9c9e1b
Vadim <varasev@gmail.com>
2019-05-03 10:29:38 +0300

    Improve `mintReward` function

--- test/mockContracts/ERC677BridgeTokenRewardableMock.sol
+++ test/mockContracts/ERC677BridgeTokenRewardableMock.sol
@@ -597,8 +597,11 @@ contract ERC677BridgeTokenRewardable is ERC677BridgeToken {
 
   function mintReward(address[] calldata _receivers, uint256[] calldata _rewards) external onlyBlockRewardContract {
     for (uint256 i = 0; i < _receivers.length; i++) {
-      address to = _receivers[i];
       uint256 amount = _rewards[i];
+
+      if (amount == 0) continue;
+
+      address to = _receivers[i];
 
       // Mint `amount` for `to`
       totalSupply_ = totalSupply_.add(amount);
