f6a14356de5b2eef3e049b977a28554d97ec7c7c
Max Alekseenko <nnwen0609@gmail.com>
2019-06-18 20:30:29 +0300

    fix solidity coverage

--- scripts/test.sh
+++ scripts/test.sh
@@ -59,7 +59,7 @@ start_ganache() {
   )
 
   if [ "$SOLIDITY_COVERAGE" = true ]; then
-    node_modules/.bin/testrpc-sc --gasLimit 0xfffffffffff --port "$ganache_port" "${accounts[@]}" > /dev/null &
+    node_modules/.bin/testrpc-sc --gasLimit 0xfffffffffff --allowUnlimitedContractSize --port "$ganache_port" "${accounts[@]}" > /dev/null &
   else
     node_modules/.bin/ganache-cli --gasLimit 0xfffffffffff --port "$ganache_port" "${accounts[@]}" > /dev/null &
   fi
