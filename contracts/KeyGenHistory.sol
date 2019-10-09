cabed75589d73191d36455a803e72bee694b694c
varasev <33550681+varasev@users.noreply.github.com>
2019-10-09 13:54:46 +0300

    Replace constructor with initParts and initAcks

--- contracts/KeyGenHistory.sol
+++ contracts/KeyGenHistory.sol
@@ -1,20 +1,31 @@
-pragma solidity 0.5.9;
+pragma solidity 0.5.12;
 pragma experimental ABIEncoderV2;
 
 
 contract KeyGenHistory {
 
     mapping(address => bytes) public parts;
-    mapping(address => bytes) public acks;
-
-    constructor(address[] memory _validators, bytes[] memory _parts, bytes[] memory _acks) public {
+    mapping(address => bytes[]) public acks;
+    address public owner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    // This is used instead of constructor
+    function initParts(address[] memory _validators, bytes[] memory _parts) public {
+        require(msg.sender == owner);
         require(_validators.length == _parts.length);
-        require(_validators.length == _acks.length);
-
+        
         for (uint256 i = 0; i < _validators.length; i++) {
             parts[_validators[i]] = _parts[i];
-            acks[_validators[i]] = _acks[i];
         }
+    }
+    
+    // This is used instead of constructor
+    function initAcks(address _validator, bytes[] memory _acks) public {
+        require(msg.sender == owner);
+        acks[_validator] = _acks;
     }
 
     function writePart(bytes memory _part) public {
@@ -29,7 +40,7 @@ contract KeyGenHistory {
         parts[msg.sender] = _part;
     }
 
-    function writeAck(bytes memory _ack) public {
+    function writeAcks(bytes[] memory _acks) public {
         // TODO: can only be called by a new validator which is elected but not yet finalized
         // or by a validator which is already in the validator set (ValidatorSet.isPendingValidator(msg.sender)
         // must return `true`).
@@ -38,7 +49,7 @@ contract KeyGenHistory {
         // (it means that the `InitiateChange` event was emitted, but the `finalizeChange`
         // function wasn't yet called).
 
-        acks[msg.sender] = _ack;
+        acks[msg.sender] = _acks;
     }
 
 }
