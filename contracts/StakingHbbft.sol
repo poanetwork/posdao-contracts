3b74c4ef3ef5c4768c12b43db5032ca690c75531
i-stam <i.stamelakos@gmail.com>
2019-11-19 17:34:59 +0000

    Pass pool parameters during StakingHbbft initialization

--- contracts/StakingHbbft.sol
+++ contracts/StakingHbbft.sol
@@ -1,33 +1,11 @@
 pragma solidity 0.5.10;
 
-import "./base/StakingAuRaTokens.sol";
-//import "./base/StakingAuRaCoins.sol";
+import "./base/StakingHbbftTokens.sol";
+//import "./base/StakingHbbftCoins.sol";
 
 
-contract StakingHbbft is StakingAuRaTokens {
+contract StakingHbbft is StakingHbbftTokens {}
 
 // Uncomment this line and comment out the above one
 // if staking in native coins is needed instead of staking in tokens:
-// contract StakingAuRa is StakingAuRaCoins {}
-
-    struct PoolInfo {
-        bytes publicKey;
-        bytes16 internetAddress;
-    }
-
-    mapping (address => PoolInfo) public poolInfo;
-
-    function setPoolInfo(address _poolAddress, bytes calldata _publicKey, bytes16 _ip) external onlyValidatorSetContract{
-        poolInfo[_poolAddress].publicKey = _publicKey;
-        poolInfo[_poolAddress].internetAddress = _ip;
-    }
-
-    function getPoolPublicKey(address _poolAddress) public view returns (bytes memory){
-        return poolInfo[_poolAddress].publicKey;
-    }
-
-    function getPoolInternetAddress(address _poolAddress) public view returns (bytes16){
-        return poolInfo[_poolAddress].internetAddress;
-    }
-
-}
+// contract StakingHbbft is StakingHbbftCoins {}
