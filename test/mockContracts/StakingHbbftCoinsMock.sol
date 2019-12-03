263f8187ea6902a52125ef948695b7333a7c480b
i-stam <i.stamelakos@gmail.com>
2019-12-03 21:34:51 +0000

    Add and fix StakingHbbftCoins related files

---
+++ test/mockContracts/StakingHbbftCoinsMock.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.5.10;
+
+import './StakingHbbftBaseMock.sol';
+import '../../contracts/base/StakingHbbftCoins.sol';
+
+
+contract StakingHbbftCoinsMock is StakingHbbftCoins, StakingHbbftBaseMock {}
