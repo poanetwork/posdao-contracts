40340d1e209aef6c8375f9bb1fd1e680a588ca03
Vadim <varasev@gmail.com>
2020-01-03 15:03:37 +0300

    Fix documenter

--- docs/website/sidebars-fix.js
+++ docs/website/sidebars-fix.js
@@ -16,9 +16,18 @@ for (const k in sidebars['docs-api']) {
 sidebarsFixed['docs-api'][' '] = ['index'];
 
 const Registry_Owned_Index = sidebarsFixed['docs-api']['root'].indexOf('Registry_Owned');
-
 if (Registry_Owned_Index >= 0) {
 	sidebarsFixed['docs-api']['root'].splice(Registry_Owned_Index, 1);
+}
+
+let Sacrifice_Index
+Sacrifice_Index = sidebarsFixed['docs-api']['base'].indexOf('base_BlockRewardAuRaBase_Sacrifice');
+if (Sacrifice_Index >= 0) {
+	sidebarsFixed['docs-api']['base'].splice(Sacrifice_Index, 1);
+}
+Sacrifice_Index = sidebarsFixed['docs-api']['base'].indexOf('base_StakingAuRaCoins_Sacrifice');
+if (Sacrifice_Index >= 0) {
+	sidebarsFixed['docs-api']['base'].splice(Sacrifice_Index, 1);
 }
 
 fs.writeFileSync(sidebarsJsonFile, JSON.stringify(sidebarsFixed, null, '  '));
